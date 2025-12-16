4aefd1f07bd32563851c4a90141e98ad69dd76fa
Dylan Marin Campo <dylanriwi.dev@gmail.com>
2025-12-16 11:35:25 -0500

    feat(domain): add auth fields to coder and company entities

--- Ilary.Domain/Entities/Company.cs
+++ Ilary.Domain/Entities/Company.cs
@@ -6,7 +6,10 @@ public class Company
     public string Name { get; set; } = string.Empty;
     public int NIT { get; set; }
     public string Description { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
     public ICollection<JobApplication> JobApplication { get; set; } = new List<JobApplication>();
+    public ICollection<Roles> Roles { get; set; } = new List<Roles>();
     public DateTime Created { get; set; }
     public DateTime Updated { get; set; }
     
---
+++ Ilary.Infrastructure/Migrations/20251216050656_AddAuthFields.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ilary.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuthFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Companies",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Companies",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Coder",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Coder");
+        }
+    }
+}
